b6a7d3844d9614817999e688f08b814530d38849
syifacts <ss4166269@gmail.com>
2025-07-15 14:44:02 +0700

    new

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,11 +1,13 @@
 import Beranda from "../views/pages/beranda";
+import Journaling from "../views/pages/journaling";
 import Tracking from "../views/pages/tracking";
 
 
 const routes = {
     '/': Beranda,
     '/beranda' : Beranda,
-    '/tracking': Tracking
+    '/tracking': Tracking,
+    '/journaling' : Journaling
 };
 
 export default routes;

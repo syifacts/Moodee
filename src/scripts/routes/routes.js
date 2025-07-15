b96e7951b4ecca6f4e66a1787594f34388763be2
fany <fanymaria225@gmail.com>
2025-07-15 14:23:29 +0700

    journ

--- src/scripts/routes/routes.js
+++ src/scripts/routes/routes.js
@@ -1,9 +1,11 @@
 import Beranda from "../views/pages/beranda";
+import Journaling from "../views/pages/journaling";
 
 
 const routes = {
     '/': Beranda,
-    '/beranda' : Beranda
+    '/beranda' : Beranda,
+    '/journaling': Journaling
 };
 
 export default routes;
